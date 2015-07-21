82623cfee4373b9e529880ca8ca94c2871f62894
Eloy Urrejola <eloyurrejola@gmail.com>
2015-07-21 23:09:11 +0200

    más...

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -15,12 +17,22 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void submitOrder(View view) {
-        display(1);
+        int quantity = 1;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
